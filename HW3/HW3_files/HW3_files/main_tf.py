8c383c70bf22ab67ee0e5969676402a4de3cb6c2
hackerrank <me@hackerrank.com>
2021-04-06 17:16:19 -0500

    first day

--- HW3/HW3_files/HW3_files/main_tf.py
+++ HW3/HW3_files/HW3_files/main_tf.py
@@ -1,5 +1,10 @@
 import tensorflow as tf
 import argparse
+from models.tensorflow import vgg_tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.optimizers import *
+from tensorflow.keras.losses import *
+from tensorflow.keras.utils import to_categorical
 
 # Argument parser
 parser = argparse.ArgumentParser(description='EE379K HW3 - Starter TensorFlow code')
@@ -16,9 +21,25 @@ random_seed = 1
 tf.random.set_seed(random_seed)
 
 # TODO: Insert your model here
-model = None
+model = vgg_tf.VGG11_tf()
+model.summary()
 
 # TODO: Load the training and testing datasets
+(X_train, y_train), (X_test, y_test)= cifar10.load_data()
+X_train = X_train.astype('float32') / 255.0
+X_test = X_test.astype('float32') / 255.0
+nb_classes = 10
+Y_train = to_categorical(y_train, nb_classes)
+Y_test = to_categorical(y_test, nb_classes)
+
+## find channel mean, std and do data normalization
+train_mean = (0.4914, 0.4822, 0.4465)
+train_std = (0.2023, 0.1994, 0.2010)
+X_train = (X_train - train_mean) / train_std
+X_test = (X_test - train_mean) / train_std
+
+print(Y_train.shape)
+
 
 # TODO: Convert the datasets to contain only float values
 
@@ -27,7 +48,8 @@ model = None
 # TODO: Encode the labels into one-hot format
 
 # TODO: Configures the model for training using compile method
-
+model.compile(optimizer="Adam", loss="categorical_crossentropy")
 # TODO: Train the model using fit method
-
+history = model.fit( X_train, Y_train, batch_size = batch_size, epochs = epochs)
+print(history.history)
 # TODO: Save the weights of the model in .ckpt format
--- HW3/HW3_files/HW3_files/models/tensorflow/vgg_tf.py
+++ HW3/HW3_files/HW3_files/models/tensorflow/vgg_tf.py
@@ -21,14 +21,8 @@ def VGG11_tf():
     model.add(Conv2D(filters=512, kernel_size=3, padding = 'same', activation = 'relu'))
     model.add(Conv2D(filters=512, kernel_size=3, padding = 'same', activation = 'relu'))
     model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
+    model.add(Flatten())
     model.add(Dense(units=512,activation='relu'))
     model.add(Dense(units=512,activation='relu'))
     model.add(Dense(units=10,activation='softmax'))
     return model
-
-
-model = VGG11_tf()
-print("Model Summary: ")
-print(model.summary())
-print("done")
